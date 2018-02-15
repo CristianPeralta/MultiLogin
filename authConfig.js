e338617d171463f202d7f32b8eb561772e9da079
Cristian Peralta <cbperaltasegura@gmail.com>
2018-02-15 09:50:11 -0500

    excluding auth

--- authConfig.js
+++
@@ -1,8 +0,0 @@
-var facebook = {
-  ID : '703238480065028',
-  Secret : '77795a9efd3aceaed76ab7f811402706',
-  URL : 'http://localhost:3000/auth/facebook/callback'
-}
-module.exports = {
-  facebook
-};
